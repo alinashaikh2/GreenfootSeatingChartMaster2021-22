d1b1ee2aeb029ba9945a476f40630a07514df2e2
alinashaikh2 <90412955+alinashaikh2@users.noreply.github.com>
2021-09-21 10:40:25 -0700

    files

---
+++ AlinaShaikh.java
@@ -0,0 +1,102 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class AlinaShaikh here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AlinaShaikh extends Student
+{
+    /**
+     * Act - do whatever the AlinaShaikh wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public AlinaShaikh(String f, String l, int r, int s) {
+        firstName=f;
+        lastName=l;
+        mySeatX=r;
+        mySeatY=s;
+        portraitFile=f.toLowerCase()+l.toLowerCase()+".jpg";    // Make sure to name your image files firstlast.jpg, all lowercase!!!
+        standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
+        soundFile=f.toLowerCase()+l.toLowerCase()+".wav";  // Make sure to name your sound files firstlast.wav, all lowercase!!!
+        setImage(portraitFile);
+        sitting=true;
+    }
+    /**
+     * Default constructor, if you don't pass in a name and seating location
+     * Pay attention to how the row and seat variables set the location of the image.  1,1 is the first cell in the upper left
+     * of the classroom.
+     */
+    public AlinaShaikh() {
+        firstName="Alina";
+        lastName="Shaikh";
+        mySeatX=1;
+        mySeatY=1;
+       // imgFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
+       portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
+       standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
+        soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".wav";
+        setImage(portraitFile);
+        sitting=true;
+    }
+    
+    public void act() 
+    {
+        // Add your action code here.
+        if(Greenfoot.mouseClicked(this)){
+          //  if (sitting){
+                sitting=false;
+                setImage(standingFile);
+                System.out.println(""); // Print a blank line to create space between any student output.
+                getName();
+                sayName(soundFile);
+            
+                myHobby("I like to time travel!");
+            // Create a "special method for your class and put the call here.  You can twirl your image, resize it, move it around, change transparancy, or a 
+            // combination of all of those types of actions, or more. Make sure to save the original image if you manipulate it, so that you can put it back.
+            // Call the sitDown() method to move back  to your seat
+            
+                circleClass();  // Kilgore Trount's special method... Please write one of your own. You can use this, but please modify it and be creative.
+           
+                sitDown();
+            }
+    }    
+    public void getName(){
+        System.out.println("My name is " + firstName + " " + lastName);
+    }
+    public void circleClass(){
+        setLocation(0,0);
+         Greenfoot.delay(10);
+        // move right
+        for (int i=1;i<=9;i++){
+            setLocation(i,0);
+            Greenfoot.delay(10);
+        }
+        // move back
+        for (int i=1;i<=5;i++){
+            setLocation(9,i);
+            Greenfoot.delay(10);
+        }      
+         // move left
+        for (int i=9;i>=0;i--){
+            setLocation(i,5);
+            Greenfoot.delay(10);
+        }      
+              // move Forward
+        for (int i=5;i>=0;i--){
+            setLocation(0,i);
+            Greenfoot.delay(10);
+        }   
+           Greenfoot.delay(20);
+           sitDown();
+    }
+     /**
+     * myHobby is one of the interfaces provided.  
+     * An interface is just a contract for the methods that you will implement in your code.  The College Board no longer
+     * tests on abstract classes and interfaces, but it is good to know about them
+     */
+     public void myHobby(String s) {
+         System.out.println(s);
+        }
+}
