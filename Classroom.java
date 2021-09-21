08c64d68866c1d88de3b840178f5f9ae1bb2174a
alinashaikh2 <90412955+alinashaikh2@users.noreply.github.com>
2021-09-21 10:42:24 -0700

    classroom

--- Classroom.java
+++ Classroom.java
@@ -142,17 +142,21 @@ public class Classroom extends World
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
      */
-    
+
     private void prepare()
     {
         createDeskLayout();
- // Each student needs to create their specific instance following the KilgoreTrout example.
- // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
- // and then clicking on the inspect text
+        // Each student needs to create their specific instance following the KilgoreTrout example.
+        // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
+        // and then clicking on the inspect text
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+
   
+        AlinaShaikh alinaShaikh = new AlinaShaikh();
+        addObject(alinaShaikh,8,7);
+        alinaShaikh.assignSeat();
     }
     
     public List<Student> getAllStudents(){
